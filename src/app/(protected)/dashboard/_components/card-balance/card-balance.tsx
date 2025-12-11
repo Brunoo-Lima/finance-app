32cdc752ab1085c650b3f2d78cc0899b3decd215
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2025-12-10 21:43:49 -0300

    feat: visible balance updated

--- src/app/(protected)/dashboard/_components/card-balance/card-balance.tsx
+++ src/app/(protected)/dashboard/_components/card-balance/card-balance.tsx
@@ -1,7 +1,12 @@
-import { ArrowUpDownIcon, EyeIcon, WalletIcon } from "lucide-react";
+"use client";
+
+import { ArrowUpDownIcon, EyeIcon, EyeOffIcon, WalletIcon } from "lucide-react";
 import s from "./_card-balance.module.scss";
+import { useState } from "react";
 
 export const CardBalance = () => {
+  const [isVisibleBalance, setIsVisibleBalance] = useState<boolean>(false);
+
   return (
     <div className={s.card__balance__container}>
       <div className={s.balance__wrapper}>
@@ -13,8 +18,24 @@ export const CardBalance = () => {
         </div>
 
         <div className={s.value__container}>
-          <h2>R$ 2.700</h2>
-          <EyeIcon />
+          <h2>
+            {isVisibleBalance
+              ? [1, 2, 3, 4, 5].map((index) => <span key={index}></span>)
+              : "R$ 2.700"}
+          </h2>
+          {isVisibleBalance ? (
+            <EyeIcon
+              size={24}
+              className={s.icon}
+              onClick={() => setIsVisibleBalance(false)}
+            />
+          ) : (
+            <EyeOffIcon
+              size={24}
+              className={s.icon}
+              onClick={() => setIsVisibleBalance(true)}
+            />
+          )}
         </div>
       </div>
 
