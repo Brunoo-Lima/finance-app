09afef14f0b7906f2a03421d0fc07646254b7cc0
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2026-01-15 22:26:59 -0300

    feat: screen register added

---
+++ src/app/(public)/cadastrar/page.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next';
+import s from './page.module.scss';
+import { RegisterForm } from '@/components/layouts/(public)/register-form/register-form';
+import Link from 'next/link';
+import { ChevronLeftIcon } from 'lucide-react';
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'Login para sua conta.',
+};
+
+export default function RegisterPage() {
+  return (
+    <section className={s.register__wrapper}>
+      <div className={s.register__container}>
+        <div className={s.register__header}>
+          <Link href={'/'}>
+            <ChevronLeftIcon size={24} color="currentColor" />
+          </Link>
+          <h1>Vamos criar sua conta</h1>
+        </div>
+
+        <RegisterForm />
+      </div>
+    </section>
+  );
+}
